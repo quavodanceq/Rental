62dec39c5acb98d6b5decaf4b5a04e7480962094
Kuat <80914126+quavodanceq@users.noreply.github.com>
2026-01-11 14:15:17 +0500

    Initial Commit

---
+++ Rental/RentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentalApp.swift
+//  Rental
+//
+//  Created by Куат Оралбеков on 11.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct RentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
